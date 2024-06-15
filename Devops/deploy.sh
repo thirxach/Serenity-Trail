34ba87a8a0b7b19afc03d917aea4e99449219770
func <func@funcma.com>
2024-06-15 16:46:20 +1200

    fix: deploy script

--- Devops/deploy.sh
+++ Devops/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # cd to the directory of the script CHANGE IT
-cd ./
+cd /root/wintec/Serenity-Trail/
 
 # Pull the latest code
 git pull
-
-# Build the Docker image
-docker build -t serenity .
 
 # Remove the old Docker container
 docker rm -f serenity
 
 # Remove the old Docker image
 docker rmi serenity
+
+# Build the Docker image
+docker build -t serenity .
 
 # Run the new Docker container
 docker run -d --name serenity serenity
