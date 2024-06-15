cea668c502d170d382e7d56c3b8a3b29b2dc6b4f
func <func@funcma.com>
2024-06-15 15:12:26 +1200

    add: phone number and delete button

--- server/web.py
+++ server/web.py
@@ -12,25 +12,32 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
     message = db.Column(db.String(200))
 
     def __repr__(self):
         return f'<Message {self.name}>'
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/trail')
 def trail():
     return render_template('Trail.html')
 
+
 @app.route('/about')
 def about():
     return render_template('About.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('Contact.html')
+
 
 @app.route('/tp', methods=['GET'])
 def get_message():
@@ -39,10 +46,11 @@ def get_message():
     db.session.commit()
     return jsonify({'message': 'Message stored'}), 201
 
+
 @app.route('/feedback', methods=['POST'])
 def post_message():
     data = request.get_json()
-    new_message = Message(name=data['name'], email=data['email'], message=data['message'])
+    new_message = Message(name=data['name'], email=data['email'], phone=data['phone'], message=data['message'])
     db.session.add(new_message)
     db.session.commit()
     return jsonify({'message': 'Message stored'}), 201
@@ -53,14 +61,26 @@ def get_messages():
     messages = Message.query.all()
     output = []
     for message in messages:
-        message_data = {'name': message.name, 'email': message.email, 'message': message.message}
+        message_data = {'name': message.name, 'email': message.email, 'phone': message.phone,
+                        'message': message.message}
         output.append(message_data)
     return jsonify(output)
 
-@app.route('/view_messages')
+
+@app.route('/admin/view_feedback')
 def view_messages():
     messages = Message.query.all()
     return render_template('feedback_dashboard.html', messages=messages)
+
+
+@app.route('/admin/delete_feedback', methods=['POST'])
+def delete_messages():
+    data = request.get_json()
+    message = Message.query.filter_by(id=data['id']).first()
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'})
+
 
 if __name__ == '__main__':
     with app.app_context():
